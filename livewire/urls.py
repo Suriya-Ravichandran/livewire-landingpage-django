199103b411b9e043777352bc7067c92cd5580d97
suriyaravichandran <rsuriya119@gmail.com>
2025-11-06 16:07:50 +0530

    update

--- livewire/urls.py
+++ livewire/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
+
+app_name="livewire"
+
 urlpatterns = [
     path("",views.index,name="index"),
+    path("about/",views.about,name="aboutus"),
+    path("oldurl/",views.oldurl,name="oldurl"),
+    path("newurl/",views.newurl,name="newurl"),
 ]
--- livewire/views.py
+++ livewire/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello this My django First Project")
+    
+    title="Home-Livewire"
+    content="Index page"
+
+    context={
+        "title":title,
+        "content":content,
+    }
+    return render(request,"index.html",context)
+
+
+def about(request):
+    title="About-Livewire"
+
+    context={
+        "title":title,
+    }
+
+    return render(request,"pages/about.html",context)
+
+
+def oldurl(request):
+    return redirect(reverse("livewire:newurl"))
+
+def newurl(request):
+    return HttpResponse("This new url")
