b6c141adf72e66ef7cdf4cc745e61bc3e6c4f275
suriyaravichandran <rsuriya119@gmail.com>
2025-11-14 16:39:02 +0530

    login signup completed

--- livewire/urls.py
+++ livewire/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("newurl/",views.newurl,name="newurl"),
     path("signin/",views.signin,name="signin"),
     path("signup/",views.signup,name="signup"),
+    path("signout/",views.signout,name="signout"),
 ]
--- livewire/views.py
+++ livewire/views.py
@@ -1,27 +1,16 @@
 from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    
-    title="Home-Livewire"
-    content="Index page"
-
-    context={
-        "title":title,
-        "content":content,
-    }
-    return render(request,"index.html",context)
+    return render(request,"index.html")
 
 
 def about(request):
-    title="About-Livewire"
-
-    context={
-        "title":title,
-    }
-
-    return render(request,"about.html",context)
+    return render(request,"about.html")
 
 
 def oldurl(request):
@@ -31,17 +20,41 @@ def newurl(request):
     return HttpResponse("This new url")
 
 def signin(request):
-    title="signin"
-
-    context={
-        "title":title
-    }
-    return render(request,'signin.html',context)
+    if request.method=='POST':
+        username=request.POST.get("name")
+        password=request.POST.get("password")
+        user= authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect("livewire:index")
+        else:
+            messages.error(request, "Invaild User or Password")
+            return redirect("livewire:signin")
+    return render(request,'signin.html')
 
 def signup(request):
-    title="signup"
+    if request.method=='POST':
+        username=request.POST.get("name")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        confirmpassword=request.POST.get("confirmpassword")
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+            return redirect("livewire:signup")
+        if password!=confirmpassword:
+            messages.error(request, "Password Not Match")
+            return redirect("livewire:signup")
+        # 2️⃣ Create the user in auth_user table
+        User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+        messages.success(request, "Signup successful! Go to login...")
+        return redirect("livewire:signup")
+    
+    return render(request,'signup.html')
 
-    context={
-        "title":title
-    }
-    return render(request,'signup.html',context)
+def signout(request):
+    logout(request)
+    return redirect("livewire:index")
